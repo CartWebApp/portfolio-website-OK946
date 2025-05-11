befd20892d19579c6759a08d1f060304ac3d7211
OK946 <owen.kolbert24@student.cart.org>
2025-05-10 17:05:03 -0700

    Finished basics for almost all pages

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById(`bird`).addEventListener(`click`, playAudio(`/audio/bird.mp3`))
+if(document.getElementById(`bird`)){document.getElementById(`bird`).addEventListener(`click`, playAudio(`/audio/bird.mp3`))}
 
 
 
@@ -6,4 +6,33 @@ function playAudio(audio) {
     let sound = new Audio(audio);
     sound.play();
     console.log(`Audio Played!`)
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("writingSlide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
